9df13d1eca7b8bc7080f72a6f00ecb76aedeb08a
BRIGHT (Bwave) <ivquestservices@gmail.com>
2023-06-21 19:20:20 +0100

    Create hostname.sh

---
+++ month-0/week-2/day-2/hostname.sh
@@ -0,0 +1,6 @@
+#Write a script that prints out "this script is running on (hostname) where the hostname is the name of the computer the script runs on
+
+#!/bin/bash
+
+
+echo "This script is running on $(hostname)"
