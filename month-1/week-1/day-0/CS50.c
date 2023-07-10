326e87c0d9d5a26b51c9b3ea1a6e572604889595
BRIGHT (Bwave) <ivquestservices@gmail.com>
2023-07-10 13:34:27 +0100

    Create CS50.c

---
+++ month-1/week-1/day-0/CS50.c
@@ -0,0 +1,13 @@
+// Here's the code for cs50 first pset. Submit hello:
+
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    
+    string name = get_string("What's your name? ");
+
+    printf("hello, %s\n", name);
+}
+
